671184574f8588b53f49ac10bec0b153736dc2a9
Robin OGER <oger.robin@gmail.com>
2023-05-25 12:00:29 +0200

    triple services

---
+++ src/components/ServiceComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <img :src="imageUrl" :alt="title" />
+    <h3>{{ title }}</h3>
+    <p>{{ content }}</p>
+  </section>
+</template>
+
+<script>
+  export default {
+    props: {
+      icon: String,
+      title: String,
+      content: String,
+    },
+    computed: {
+      imageUrl: function () {
+        // Crée un chemin absolu vers l'image
+        return new URL(this.icon, import.meta.url).href
+      },
+    },
+  }
+</script>
+
+<style scoped>
+  section {
+    padding: 1rem;
+    text-align: center;
+  }
+  img {
+    height: 3rem;
+  }
+  h3 {
+    margin: 1rem auto;
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -60,6 +60,23 @@
         repudiandae repellat delectus optio ex.
       </p>
     </DoubleColumnsContent>
+    <section class="services">
+      <ServiceComponent
+        icon="../assets/bowl.png"
+        title="Service 1"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi quisquam aperiam sed quo!"
+      />
+      <ServiceComponent
+        icon="../assets/bed.png"
+        title="Service 2"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi quisquam aperiam sed quo!"
+      />
+      <ServiceComponent
+        icon="../assets/yinyang.png"
+        title="Service 3"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi quisquam aperiam sed quo!"
+      />
+    </section>
   </main>
 </template>
 
@@ -67,7 +84,30 @@
   import VideoComponent from "../components/VideoComponent.vue"
   import WelcomeComponent from "../components/WelcomeComponent.vue"
   import DoubleColumnsContent from "../components/DoubleColumnsContent.vue"
+  import ServiceComponent from "../components/ServiceComponent.vue"
+  /**
+   * Créer un component ServiceComponent.vue
+   * Props: - icon: lien vers une icône
+   *        - title: titre
+   *        - content: contenu
+   * En afficher 3 côte à côte sur la HomeView comme sur
+   * la maquette
+   */
   export default {
-    components: { DoubleColumnsContent, VideoComponent, WelcomeComponent },
+    components: {
+      DoubleColumnsContent,
+      VideoComponent,
+      WelcomeComponent,
+      ServiceComponent,
+    },
   }
 </script>
+
+<style scoped>
+  .services {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    background: #faf4ef;
+    margin: 4rem auto;
+  }
+</style>
